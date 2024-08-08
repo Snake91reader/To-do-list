1f27e0f8b49e49d6e216b76c3e12c76474803f48
Snake91reader <sks5558877@gmail.com>
2024-08-08 17:19:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ const taskInput = document.querySelector(".task-input input"),
   clearAll = document.querySelector(".clear-btn"),
   taskBox = document.querySelector(".task-box");
 
-// Initialize variables
 let editId,
   isEditTask = false,
   todos = JSON.parse(localStorage.getItem("todo-list"));
 
-// Event listeners for filter buttons
 filters.forEach(btn => {
   btn.addEventListener("click", () => {
     document.querySelector("span.active").classList.remove("active");
@@ -17,7 +15,6 @@ filters.forEach(btn => {
   });
 });
 
-// Function to display tasks based on filter
 function showTodo(filter) {
   let liTag = "";
   if (todos) {
@@ -43,25 +40,20 @@ function showTodo(filter) {
     });
   }
 
-  // Update task box with tasks or a message if no tasks
   taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
   let checkTask = taskBox.querySelectorAll(".task");
 
-  // Toggle clear all button based on task presence
   !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
 
-  // Add or remove overflow class based on task box height
   taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
 }
 
 showTodo("all");
 
-// Function to display menu for task actions
 function showMenu(selectedTask) {
   let menuDiv = selectedTask.parentElement.lastElementChild;
   menuDiv.classList.add("show");
 
-  // Hide menu if clicked outside
   document.addEventListener("click", e => {
     if (e.target.tagName != "I" || e.target != selectedTask) {
       menuDiv.classList.remove("show");
@@ -69,7 +61,6 @@ function showMenu(selectedTask) {
   });
 }
 
-// Function to update task status (completed or pending)
 function updateStatus(selectedTask) {
   let taskName = selectedTask.parentElement.lastElementChild;
   if (selectedTask.checked) {
@@ -82,7 +73,6 @@ function updateStatus(selectedTask) {
   localStorage.setItem("todo-list", JSON.stringify(todos))
 }
 
-// Function to prepare for editing a task
 function editTask(taskId, textName) {
   editId = taskId;
   isEditTask = true;
@@ -91,7 +81,6 @@ function editTask(taskId, textName) {
   taskInput.classList.add("active");
 }
 
-// Function to delete a task
 function deleteTask(deleteId, filter) {
   isEditTask = false;
   todos.splice(deleteId, 1);
@@ -99,7 +88,6 @@ function deleteTask(deleteId, filter) {
   showTodo(filter);
 }
 
-// Event listener for clear all button
 clearAll.addEventListener("click", () => {
   isEditTask = false;
   todos.splice(0, todos.length);
@@ -107,7 +95,6 @@ clearAll.addEventListener("click", () => {
   showTodo()
 });
 
-// Event listener for task input
 taskInput.addEventListener("keyup", e => {
   let userTask = taskInput.value.trim();
   if (e.key == "Enter" && userTask) {
